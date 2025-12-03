0230d9196475ba31341d0c58fe6c2b30c9287be8
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-12-03 21:32:39 +0300

    DErs3

---
+++ 4. Ders/DegiskenlerProjesi/DegiskenlerProjesi/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Merhaba Dünya!");
+
+Console.WriteLine("Merhaba Tugay!");
+
+// degisken tipi - degiskenismi - = degeri ;
+string isim1 = "tugay";
+string yazi1 = "Bu c# eğitimidir. Bu eğitim kapsamında yazılım temellerini öğreneceğiz.";
+
+
+
+Console.WriteLine("Hoşgeldin " +  isim1);
+
+Console.WriteLine(yazi1);
+
+
+
+// degisken tipi - degiskenismi - = degeri ;
+int sayi1 = 12;
+
+
+Console.WriteLine(sayi1);
+
+Console.WriteLine(5 + 10);
+Console.WriteLine(10 - 5);
+Console.WriteLine(10 * 5);
+Console.WriteLine(10 / 2);
+
+sayi1 = 50;
+
+int sayi2 = 10;
+
+Console.WriteLine(sayi1 + sayi2);
+
+
