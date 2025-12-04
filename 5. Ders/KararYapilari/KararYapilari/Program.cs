a155f6b782d81fb78ce11abf3b1c057906b183a6
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-12-04 20:55:35 +0300

    ders

---
+++ 5. Ders/KararYapilari/KararYapilari/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("5. Derse Hoş Geldiniz!");
+
+Console.WriteLine("Karar Yapıları");
+
+string isim1 = "tugay";
+string isim2 = "izel";
+
+int yas = 20;
+
+
+Console.WriteLine(isim1 + " " + yas);
+
+// degisken tipi - degiskenismi - = degeri ;
+bool admin = true; // doğru
+bool uygun = false; // yanlış
+
+yas = 25;
+
+if (yas > 18)
+{
+    Console.WriteLine("18 yaşından büyüksün!");
+}
+else
+{
+    Console.WriteLine("18 yaşından küçüksünüz!");
+}
+
+if (isim1 == "tugaytuna") 
+{
+    Console.WriteLine("Hoşgeldin admin!");
+}else
+{
+    Console.WriteLine("Hoşgeldin misafir kullanıcı");
+    isim1 = "misafir";
+}
