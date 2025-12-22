72a640e70035bd43c6085b7410a1bf4140fc2e65
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-12-22 20:54:17 +0300

    Odev 3

--- 13.Ders/Ödev2/Odev2/Program.cs
+++ 13.Ders/Ödev2/Odev2/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ödev 2");
+
+
+// kullanıcıdan kaç adet sayı girmesini istediğini sor
+// girilen sayı adeti kadar int dizisi oluştur
+// çift sayıları yazdır
+// tek sayıları yazdır
+
+
+Console.WriteLine("Kaç adet sayı girmek istiyorsunuz:");
+int adet = int.Parse(Console.ReadLine());
+
+int[] dizi1 = new int[adet];
+
+for (int i = 0; i < dizi1.Length; i++)
+{
+    Console.WriteLine((i + 1) +". sayıyı lütfen giriniz:");
+    dizi1[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Çift Sayılar");
+foreach (int item in dizi1)
+{
+    if (item % 2 == 0)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+Console.WriteLine("Tek Sayılar");
+foreach (int item in dizi1)
+{
+    if (item % 2 != 0)
+    {
+        Console.WriteLine(item);
+    }
+}
