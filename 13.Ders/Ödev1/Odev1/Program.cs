aa9875513412d8fe05923a0c329cd489a2ab35de
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-12-22 19:53:11 +0300

    Odev1

--- 13.Ders/Ödev1/Odev1/Program.cs
+++ 13.Ders/Ödev1/Odev1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime;
+
+Console.WriteLine("Ödev 1");
+
+
+int[] sayilar = { 5, 21, 7, 43, 11, 34, 55, 67, 20, 10 };
+
+// tüm elemanların ekrana yazılması
+// tüm elemanların toplanması ve ekrana yazdırılması
+// tüm elemanların ortalamasının bulunması ve ekrana yazdırılması
+
+int toplam = 0;
+for (int i = 0; i < sayilar.Length; i++)
+{
+    Console.WriteLine(sayilar[i]);
+    //toplam = sayilar[i] + toplam;
+    toplam += sayilar[i];
+}
+
+double ortalama = (double)toplam / (double)sayilar.Length;
+
+Console.WriteLine("Sayıların toplamı: " + toplam);
+Console.WriteLine("Sayıların ortalaması: " + ortalama);
