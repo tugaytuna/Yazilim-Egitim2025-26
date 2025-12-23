37c1e98645b3e4e3d39270891f42a2257b64852b
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-12-23 21:54:37 +0300

    Listeler

---
+++ 14.Ders/Listeler/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Listeler");
+
+string isim = "tugay";
+int sayi1 = 22;
+
+// 1. Yöntem
+//int[] dizi1 = new int[2];
+//dizi1[0] = 15;
+//dizi1[1] = sayi1;
+
+// 2. Yöntem
+int[] dizi1 = { 15, sayi1 };
+
+foreach (int eleman in dizi1)
+{
+    Console.WriteLine(eleman);
+}
+
+for (int i = 0; i < dizi1.Length; i++)
+{
+    Console.WriteLine(dizi1[i]);
+}
+
+Console.WriteLine(dizi1[0]);
---
+++ 14.Ders/Listeler2/Listeler2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Listeler-2");
+
+int sayi1 = 22;
+
+List<int> sayilarListesi = new List<int>();
+
+sayilarListesi.Add(48);
+sayilarListesi.Add(sayi1);
+sayilarListesi.Add(55);
+sayilarListesi.Add(22);
+
+Console.WriteLine(sayilarListesi[0]);
+
+foreach (int item in sayilarListesi)
+{
+    Console.WriteLine(item);
+}
+
+
+// Elemanın değeri yazarak silebiliyoruz.
+//sayilarListesi.Remove(22);
+
+// Index olarak silme işlemi yapar
+//sayilarListesi.RemoveAt(0);
+
+// Dizideki en son elemanı siler
+//sayilarListesi.RemoveAt(sayilarListesi.Count - 1);
+
+// Bir koşula göre silme işlemi
+//sayilarListesi.RemoveAll(item => item > 25);
+
+//Tüm listeyi temizler/sıfırlar
+//sayilarListesi.Clear();
+
+sayilarListesi.Insert(2,10);
+
+
+sayilarListesi.AddRange(new List<int>() { 5, 47, 11 });
+
+Console.WriteLine("For döngüsü ile tüm liste ekrana yazdırılır!");
+for (int i = 0; i < sayilarListesi.Count; i++)
+{
+    Console.WriteLine(sayilarListesi[i]);
+}
+
+List<string> adminler = new List<string>() { "tugay", "tuna", "ahmet" };
+string[] soyisimler = new string[] { "tuna", "ahmet", "özkan" };
+
+adminler.Add("yeniAdmin");
+
+//adminler.Remove("tugay");
+
+
+
+Console.WriteLine("Silmek istediğiniz kullanıcının adını giriniz");
+string silKullanici = Console.ReadLine();
+
+adminler.Remove(silKullanici);
+
+
+Console.WriteLine("Tüm adminler listeleniyor");
+foreach (string isim in adminler)
+{
+    Console.WriteLine(isim);
+}
