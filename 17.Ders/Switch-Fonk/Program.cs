9f6872041c36d7250b3d711f0f71bb9e51b94f27
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-12-29 21:21:28 +0300

    S

--- 17.Ders/Switch-Fonk/Program.cs
+++ 17.Ders/Switch-Fonk/Program.cs
@@ -2,6 +2,59 @@
 
 
 
+string sifre = "";
+
+do
+{
+    Console.WriteLine("Lütfen şifreyi giriniz:");
+    sifre = Console.ReadLine();
+   
+    
+} while (sifre != "1234");
+
+
+int sayi1 = 20;
+
+Console.WriteLine("Sayımız: " + sayi1);
+Console.WriteLine("2 ile çarpmak için '1'e basın");
+Console.WriteLine("50 ile toplamak için '2'e basın");
+Console.WriteLine("2'ye bölmek için '3'e basın");
+
+string secim = Console.ReadLine();
+
+
+//if (secim == "1")
+//{
+//    Console.WriteLine(sayi1 * 2);
+//}else if (secim == "2")
+//{
+//    Console.WriteLine(50 + sayi1);
+//}else if (secim == "3")
+//{
+//    Console.WriteLine(sayi1 / 2);
+//}else
+//{
+//    Console.WriteLine("Hatalı tuşlama yaptınız!");
+//}
+
+switch (secim)
+{
+    case "1":
+        Console.WriteLine(sayi1 * 2);
+        break;
+    case "2":
+        Console.WriteLine(50 + sayi1);
+        break;
+    case "3":
+        Console.WriteLine(sayi1 / 2);
+        break;
+    case "4":
+        Console.WriteLine(sayi1 * sayi1);
+        break;
+    default:
+        Console.WriteLine("Hatalı tuşlama yaptınız!");
+        break;
+}
 
 
 
