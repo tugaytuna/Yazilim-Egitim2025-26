8aa22ad8d0583ba3f66f4d348173897b4fa9ce91
Tugay Tuna <tugaytuna@outlook.com.tr>
2026-01-08 21:34:42 +0300

    ders22

---
+++ 22.Ders/DongulerOrnek/DongulerOrnek1/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Örnek 1");
+
+
+
+string isimDuzenleme(string isim, string soyisim)
+{
+    string fullname = isim + " " + soyisim.ToUpper();
+    return fullname;
+}
+
+ConsoleColor kirmizi = ConsoleColor.Red;
+ConsoleColor sari = ConsoleColor.Yellow;
+
+void hosgeldinMesajı(string isim, string soyisim)
+{
+    string tamisim = isimDuzenleme(isim, soyisim);
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Hoşgldiniz " + tamisim);
+    Console.ForegroundColor = ConsoleColor.White;
+
+    Console.BackgroundColor = ConsoleColor.Magenta;
+
+    Console.ForegroundColor = kirmizi;
+    Console.Write(isim);
+    Console.ForegroundColor = sari;
+    Console.Write(soyisim);
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("");
+}
+
+
+Console.WriteLine("Lütfen isminizi giriniz: "); 
+string kisim = Console.ReadLine();
+
+Console.WriteLine("Şimdi de soyisminizi giriniz:");
+string ksoyisim = Console.ReadLine();
+
+hosgeldinMesajı(kisim, ksoyisim);
+
+
+Console.WriteLine(isimDuzenleme(kisim,ksoyisim));
---
+++ 22.Ders/DongulerOrnek/DongulerOrnek2/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("Döngüler Örnek 2");
+
+
+// 1'den 50'ye kadar olan çift sayıların karelerini ekrana yazdırın (metot kullanarak)
+
+List<int> ciftSayilar = new List<int>();
+List<int> tekSayilar = new List<int>();
+
+
+
+// 1'den 50'ye kadar olan çift sayıları listeye ekle
+for (int i = 0; i < 50; i++)
+{
+    if (i % 2 == 0) // çift ise
+    {
+        ciftSayilar.Add(i);
+    }
+}
+
+for (int i = 0; i < 50; i++)
+{
+    if (i % 2 != 0) // tek ise
+    {
+        tekSayilar.Add(i);
+    }
+}
+
+
+void kareHesaplama(string secim)
+{
+    switch (secim)
+    {
+        case "1":
+            foreach (int item in ciftSayilar)
+            {
+                Console.WriteLine(item * item);
+            }
+            break;
+        case "2":
+            foreach (int item in tekSayilar)
+            {
+                Console.WriteLine(item * item);
+            }
+            break;
+    }
+}
+
+Console.WriteLine("Çift ise 1'e, Tek ise 2'ye basın");
+string tercih = Console.ReadLine();
+
+kareHesaplama(tercih);
+
+// Parametre olarak liste.
+// Tekler kırmızı, çiftler yeşil gözüksün.
