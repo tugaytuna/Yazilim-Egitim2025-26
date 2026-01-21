660d9beaa8b9bc5955ea8a463ae0bb7d017046be
Tugay Tuna <tugaytuna@outlook.com.tr>
2026-01-21 20:10:37 +0300

    Update Program.cs

--- 29.Ders/OOPKalitimVe/Program.cs
+++ 29.Ders/OOPKalitimVe/Program.cs
@@ -8,8 +8,51 @@ namespace OOPKalitimVe
 {
     internal class Program
     {
+        public class Kisi
+        {
+            public string Ad { get; set; }
+            public int Yas { get; set; }
+
+
+            public void BilgiYazdir()
+            {
+                Console.WriteLine("Bu kişinin adı: " + Ad);
+                Console.WriteLine("Bu kişinin yaşı: " + Yas);
+            }
+
+        }
+
+        public class Ogrenci
+        {
+            public string Ad { get; set; }
+            public int Yas { get; set; }
+
+            public int OkulNo { get; set; }
+        }
+
+        public class Ogretmen
+        {
+            public string Ad { get; set; }
+            public int Yas { get; set; }
+            public string Brans { get; set; }
+        }
+
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Bu uygulamada Kişi -> Öğrenci ve Öğretmen nesne örnekli bir proje yapacağız.");
+
+            Kisi kisi1 = new Kisi() { Ad = "Tugay", Yas = 18 };
+
+            Ogrenci ogrenci1 = new Ogrenci();
+            ogrenci1.Ad = "Elif";
+            ogrenci1.Yas = 19;
+            ogrenci1.OkulNo = 359;
+
+
+
+
         }
     }
 }
