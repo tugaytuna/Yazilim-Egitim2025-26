96d7503c801e18410a8e49a5c5a7bd8a6cb1c199
Tugay Tuna <tugaytuna@outlook.com.tr>
2026-01-22 21:54:48 +0300

    Update

--- 30.Ders/22OcakOOPDevam/Program.cs
+++ 30.Ders/22OcakOOPDevam/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,90 @@ namespace _22OcakOOPDevam
 {
     internal class Program
     {
+        public class Hayvan
+        {
+            public string Cinsi { get; set; }
+            public int Yas { get; set; }
+
+
+            public virtual void SesCıkar()
+            {
+                Console.WriteLine("Hayvan Sesi!");
+            }
+
+
+        }
+
+        public class Kedi : Hayvan
+        {
+            public int KediMamaStok { get; set; }
+
+
+            public override void SesCıkar()
+            {
+                Console.WriteLine("Miyav miyav!");
+            }
+
+        }
+
+        public class Kopek : Hayvan
+        {
+            public int KopekMamaStok { get; set; }
+
+            public override void SesCıkar()
+            {
+                Console.WriteLine("Hav hav!");
+            }
+
+        }
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Uygulamaya hoş geldiniz!");
+
+            Hayvan hayvan1 = new Hayvan();
+            hayvan1.Cinsi = "Diğer";
+            hayvan1.Yas = 4;
+
+            Kedi kedi1 = new Kedi();
+            kedi1.Cinsi = "British";
+            kedi1.Yas = 5;
+            kedi1.KediMamaStok = 200;
+
+            Kopek kopek1 = new Kopek();
+            kopek1.Cinsi = "Golden";
+            kopek1.Yas = 6;
+            kopek1.KopekMamaStok = 800;
+
+            Kedi kedi2 = new Kedi();
+            kedi2.Cinsi = "Tekir";
+            kedi2.Yas = 7;
+            kedi2.KediMamaStok = 400;
+
+            Hayvan hayvan2 = new Hayvan() { Cinsi = "İnek", Yas = 1 };
+
+
+            List<Hayvan> hayvanlar = new List<Hayvan>();
+            hayvanlar.Add(hayvan1);
+            hayvanlar.Add(kedi1);
+            hayvanlar.Add(kopek1);
+            hayvanlar.Add(kedi2);
+            hayvanlar.Add(hayvan2);
+
+
+
+
+           
+
+            foreach (Hayvan item in hayvanlar)
+            {
+                Console.WriteLine(item.Cinsi);
+                item.SesCıkar();
+            }
+
+
+
         }
     }
 }
---
+++ 30.Ders/OOPOrnek1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPOrnek1
+{
+    internal class Program
+    {
+        public class Personel
+        {
+            public string Isim { get; set; }
+            public string Soyisim { get; set; }
+            public int Yas { get; set; }
+
+
+            public virtual void BilgiYazdir()
+            {
+                Console.WriteLine("Kişinin adı - soyadı: " + Isim + " " + Soyisim);
+                Console.WriteLine("Kişinin yaşı: " + Yas);
+            }
+
+
+
+        }
+
+        public class Ogrenci : Personel
+        {
+            public int OkulNumarasi { get; set; }
+
+            public override void BilgiYazdir()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Öğrencinin adı - soyadı: " + Isim + " " + Soyisim);
+                Console.WriteLine("Öğrencinin okul numarası: " + OkulNumarasi);
+                Console.ResetColor();
+            }
+        }
+
+        public class Ogretmen : Personel
+        {
+            public string Brans { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Uygulamaya hoş geldiniz");
+
+            Personel personel1 = new Personel() { Isim = "Ahmet", Soyisim = "Yılmaz", Yas = 44 };
+            Ogrenci ogrenci1 = new Ogrenci() { Isim = "Samet", Soyisim = "Çakan", Yas = 20, OkulNumarasi = 198 };
+            Ogretmen ogretmen1 = new Ogretmen() { Isim = "Tugay", Soyisim = "Tuna", Yas = 30, Brans = "Yazılım" };
+            Ogrenci ogrenci2 = new Ogrenci() { Isim = "Yusuf", Soyisim = "Balcı", Yas = 18, OkulNumarasi = 548 };
+
+
+            List<Personel> personeller = new List<Personel>();
+            personeller.Add(personel1);
+            personeller.Add(ogrenci1);
+            personeller.Add(ogretmen1); 
+            personeller.Add(ogrenci2);
+
+
+            foreach (Personel prs in personeller)
+            {
+                prs.BilgiYazdir();
+            }
+
+        }
+    }
+}
