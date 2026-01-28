de72dd167cc7d0180afe620bc027d0276f6b4e57
Tugay Tuna <tugaytuna@outlook.com.tr>
2026-01-28 21:57:44 +0300

    Ders33

---
+++ 33.Ders/FormUygulamalari/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace FormUygulamalari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtYeniKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.txtYeniSifre = new System.Windows.Forms.TextBox();
+            this.txtYeniSifreTekrar = new System.Windows.Forms.TextBox();
+            this.btnKayitOl = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnGirisYap);
+            this.groupBox1.Controls.Add(this.txtSifre);
+            this.groupBox1.Controls.Add(this.txtKullaniciAdi);
+            this.groupBox1.Location = new System.Drawing.Point(29, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(251, 340);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Giriş Yap";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Şifre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Location = new System.Drawing.Point(15, 130);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(215, 30);
+            this.btnGirisYap.TabIndex = 2;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            this.btnGirisYap.Click += new System.EventHandler(this.btnGirisYap_Click);
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(18, 104);
+            this.txtSifre.MaxLength = 16;
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(212, 20);
+            this.txtSifre.TabIndex = 1;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(15, 64);
+            this.txtKullaniciAdi.MaxLength = 16;
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(215, 20);
+            this.txtKullaniciAdi.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.btnKayitOl);
+            this.groupBox2.Controls.Add(this.txtYeniSifreTekrar);
+            this.groupBox2.Controls.Add(this.txtYeniSifre);
+            this.groupBox2.Controls.Add(this.txtYeniKullaniciAdi);
+            this.groupBox2.Location = new System.Drawing.Point(308, 42);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(313, 339);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kayıt Ol";
+            // 
+            // txtYeniKullaniciAdi
+            // 
+            this.txtYeniKullaniciAdi.Location = new System.Drawing.Point(27, 63);
+            this.txtYeniKullaniciAdi.Name = "txtYeniKullaniciAdi";
+            this.txtYeniKullaniciAdi.Size = new System.Drawing.Size(232, 20);
+            this.txtYeniKullaniciAdi.TabIndex = 0;
+            // 
+            // txtYeniSifre
+            // 
+            this.txtYeniSifre.Location = new System.Drawing.Point(27, 103);
+            this.txtYeniSifre.Name = "txtYeniSifre";
+            this.txtYeniSifre.PasswordChar = '*';
+            this.txtYeniSifre.Size = new System.Drawing.Size(232, 20);
+            this.txtYeniSifre.TabIndex = 1;
+            // 
+            // txtYeniSifreTekrar
+            // 
+            this.txtYeniSifreTekrar.Location = new System.Drawing.Point(27, 145);
+            this.txtYeniSifreTekrar.Name = "txtYeniSifreTekrar";
+            this.txtYeniSifreTekrar.PasswordChar = '*';
+            this.txtYeniSifreTekrar.Size = new System.Drawing.Size(232, 20);
+            this.txtYeniSifreTekrar.TabIndex = 2;
+            // 
+            // btnKayitOl
+            // 
+            this.btnKayitOl.Location = new System.Drawing.Point(27, 192);
+            this.btnKayitOl.Name = "btnKayitOl";
+            this.btnKayitOl.Size = new System.Drawing.Size(232, 37);
+            this.btnKayitOl.TabIndex = 3;
+            this.btnKayitOl.Text = "Kayıt Ol";
+            this.btnKayitOl.UseVisualStyleBackColor = true;
+            this.btnKayitOl.Click += new System.EventHandler(this.btnKayitOl_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kullanıcı Adı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Şifre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "ŞifreTekrar";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(27, 171);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(203, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Kullanıcı sözleşmesini kabul ediyorum.";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGirisYap;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnKayitOl;
+        private System.Windows.Forms.TextBox txtYeniSifreTekrar;
+        private System.Windows.Forms.TextBox txtYeniSifre;
+        private System.Windows.Forms.TextBox txtYeniKullaniciAdi;
+    }
+}
+
---
+++ 33.Ders/FormUygulamalari/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormUygulamalari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Font = new Font("Microsoft Sans Serif", 30);
+        }
+
+        public class Kullanici
+        {
+            public string KullaniciAdi { get; set; }
+            public string KullaniciSifre { get; set; }
+        }
+
+
+        List<Kullanici> kullanicilar = new List<Kullanici>() { new Kullanici() { KullaniciAdi = "tugay", KullaniciSifre = "1234" }, new Kullanici() { KullaniciAdi = "samet", KullaniciSifre = "5432" } };
+
+        private void btnGirisYap_Click(object sender, EventArgs e)
+        {
+            string kullaniciAdi = txtKullaniciAdi.Text;
+            string sifre = txtSifre.Text;
+
+            foreach (Kullanici user in kullanicilar)
+            {
+                if (kullaniciAdi == user.KullaniciAdi && sifre == user.KullaniciSifre)
+                {
+                    MessageBox.Show("Giriş Yapıldı!");
+                    groupBox1.Visible = false;
+                    groupBox2.Visible = false;
+                    //this.Close();
+                }
+            }
+
+        }
+
+        private void btnKayitOl_Click(object sender, EventArgs e)
+        {
+            Kullanici yeniKullanici = new Kullanici();
+
+            if (txtYeniSifre.Text == txtYeniSifreTekrar.Text)
+            {
+                yeniKullanici.KullaniciAdi = txtYeniKullaniciAdi.Text;
+                yeniKullanici.KullaniciSifre = txtYeniSifre.Text;
+                kullanicilar.Add(yeniKullanici);
+                MessageBox.Show("Kullanıcı başarıyla oluşturuldu.");
+            }
+            else
+            {
+                MessageBox.Show("Girdiğiniz şifreler aynı değil!");
+            }
+            
+        }
+    }
+}
