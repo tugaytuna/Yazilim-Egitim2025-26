486c66bf194007c4db3da26d5c9151dc17be7f7c
Tugay Tuna <tugaytuna@outlook.com.tr>
2026-01-30 11:45:05 +0300

    34.ders29Ocak

---
+++ 34.Ders/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace FormUygulamalari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chbKullaniciSozlesme = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnKayitOl = new System.Windows.Forms.Button();
+            this.txtYeniSifreTekrar = new System.Windows.Forms.TextBox();
+            this.txtYeniSifre = new System.Windows.Forms.TextBox();
+            this.txtYeniKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.btnCikis = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnGirisYap);
+            this.groupBox1.Controls.Add(this.txtSifre);
+            this.groupBox1.Controls.Add(this.txtKullaniciAdi);
+            this.groupBox1.Location = new System.Drawing.Point(58, 79);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Size = new System.Drawing.Size(502, 654);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Giriş Yap";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 169);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Şifre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 92);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Location = new System.Drawing.Point(30, 250);
+            this.btnGirisYap.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(430, 58);
+            this.btnGirisYap.TabIndex = 2;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            this.btnGirisYap.Click += new System.EventHandler(this.btnGirisYap_Click);
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(36, 200);
+            this.txtSifre.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtSifre.MaxLength = 16;
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(420, 31);
+            this.txtSifre.TabIndex = 1;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(30, 123);
+            this.txtKullaniciAdi.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtKullaniciAdi.MaxLength = 16;
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(426, 31);
+            this.txtKullaniciAdi.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.chbKullaniciSozlesme);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.btnKayitOl);
+            this.groupBox2.Controls.Add(this.txtYeniSifreTekrar);
+            this.groupBox2.Controls.Add(this.txtYeniSifre);
+            this.groupBox2.Controls.Add(this.txtYeniKullaniciAdi);
+            this.groupBox2.Location = new System.Drawing.Point(616, 81);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox2.Size = new System.Drawing.Size(626, 652);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kayıt Ol";
+            // 
+            // chbKullaniciSozlesme
+            // 
+            this.chbKullaniciSozlesme.AutoSize = true;
+            this.chbKullaniciSozlesme.Location = new System.Drawing.Point(43, 372);
+            this.chbKullaniciSozlesme.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.chbKullaniciSozlesme.Name = "chbKullaniciSozlesme";
+            this.chbKullaniciSozlesme.Size = new System.Drawing.Size(413, 29);
+            this.chbKullaniciSozlesme.TabIndex = 7;
+            this.chbKullaniciSozlesme.Text = "Kullanıcı sözleşmesini kabul ediyorum.";
+            this.chbKullaniciSozlesme.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 298);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "ŞifreTekrar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 217);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Şifre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 140);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kullanıcı Adı:";
+            // 
+            // btnKayitOl
+            // 
+            this.btnKayitOl.Location = new System.Drawing.Point(43, 412);
+            this.btnKayitOl.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnKayitOl.Name = "btnKayitOl";
+            this.btnKayitOl.Size = new System.Drawing.Size(464, 71);
+            this.btnKayitOl.TabIndex = 3;
+            this.btnKayitOl.Text = "Kayıt Ol";
+            this.btnKayitOl.UseVisualStyleBackColor = true;
+            this.btnKayitOl.Click += new System.EventHandler(this.btnKayitOl_Click);
+            // 
+            // txtYeniSifreTekrar
+            // 
+            this.txtYeniSifreTekrar.Location = new System.Drawing.Point(43, 329);
+            this.txtYeniSifreTekrar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtYeniSifreTekrar.Name = "txtYeniSifreTekrar";
+            this.txtYeniSifreTekrar.PasswordChar = '*';
+            this.txtYeniSifreTekrar.Size = new System.Drawing.Size(460, 31);
+            this.txtYeniSifreTekrar.TabIndex = 2;
+            // 
+            // txtYeniSifre
+            // 
+            this.txtYeniSifre.Location = new System.Drawing.Point(43, 248);
+            this.txtYeniSifre.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtYeniSifre.Name = "txtYeniSifre";
+            this.txtYeniSifre.PasswordChar = '*';
+            this.txtYeniSifre.Size = new System.Drawing.Size(460, 31);
+            this.txtYeniSifre.TabIndex = 1;
+            // 
+            // txtYeniKullaniciAdi
+            // 
+            this.txtYeniKullaniciAdi.Location = new System.Drawing.Point(43, 171);
+            this.txtYeniKullaniciAdi.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtYeniKullaniciAdi.Name = "txtYeniKullaniciAdi";
+            this.txtYeniKullaniciAdi.Size = new System.Drawing.Size(460, 31);
+            this.txtYeniKullaniciAdi.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(43, 98);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(118, 29);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.Text = "Öğrenci";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(167, 98);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(137, 29);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.Text = "Öğretmen";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.AutoSize = true;
+            this.btnCikis.Location = new System.Drawing.Point(1438, 94);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(104, 25);
+            this.btnCikis.TabIndex = 2;
+            this.btnCikis.TabStop = true;
+            this.btnCikis.Text = "Çıkış Yap";
+            this.btnCikis.Visible = false;
+            this.btnCikis.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.btnCikis_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FormUygulamalari.Properties.Resources.okul;
+            this.pictureBox1.Location = new System.Drawing.Point(1277, 183);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(455, 267);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(817, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(340, 49);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1768, 865);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGirisYap;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chbKullaniciSozlesme;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnKayitOl;
+        private System.Windows.Forms.TextBox txtYeniSifreTekrar;
+        private System.Windows.Forms.TextBox txtYeniSifre;
+        private System.Windows.Forms.TextBox txtYeniKullaniciAdi;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.LinkLabel btnCikis;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 34.Ders/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormUygulamalari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Font = new Font("Microsoft Sans Serif", 30);
+        }
+
+        public class Kullanici
+        {
+            public string KullaniciAdi { get; set; }
+            public string KullaniciSifre { get; set; }
+            public string Meslek { get; set; }
+        }
+
+
+        List<Kullanici> kullanicilar = new List<Kullanici>() { new Kullanici() { KullaniciAdi = "tugay", KullaniciSifre = "1234", Meslek = "Öğretmen" }, new Kullanici() { KullaniciAdi = "samet", KullaniciSifre = "5432", Meslek="Öğrenci" } };
+
+        private void btnGirisYap_Click(object sender, EventArgs e)
+        {
+            string kullaniciAdi = txtKullaniciAdi.Text;
+            string sifre = txtSifre.Text;
+
+            foreach (Kullanici user in kullanicilar)
+            {
+                if (kullaniciAdi == user.KullaniciAdi && sifre == user.KullaniciSifre)
+                {
+                    MessageBox.Show("Giriş Yapıldı!");
+                    groupBox1.Visible = false;
+                    groupBox2.Visible = false;
+                    btnCikis.Visible = true;
+                    Temizle();
+                    //this.Close();
+                }
+            }
+
+        }
+
+
+        public void Temizle()
+        {
+            //giriş yap
+            txtKullaniciAdi.Text = "";
+            txtSifre.Text = "";
+
+            // yeni kayıt
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+           
+
+            txtYeniKullaniciAdi.Text = "";
+            //txtYeniKullaniciAdi.Text = string.Empty;
+            //txtKullaniciAdi.Clear();
+            txtYeniSifre.Text = "";
+            txtYeniSifreTekrar.Text = "";
+            chbKullaniciSozlesme.Checked = false;
+        }
+
+
+        private void btnKayitOl_Click(object sender, EventArgs e)
+        {
+            Kullanici yeniKullanici = new Kullanici();
+
+            if (chbKullaniciSozlesme.Checked)
+            {
+                if (txtYeniSifre.Text == txtYeniSifreTekrar.Text)
+                {
+                    yeniKullanici.KullaniciAdi = txtYeniKullaniciAdi.Text;
+                    yeniKullanici.KullaniciSifre = txtYeniSifre.Text;
+
+                    if (radioButton1.Checked)
+                    {
+                        yeniKullanici.Meslek = "Öğrenci";
+                        kullanicilar.Add(yeniKullanici);
+                        MessageBox.Show("Kullanıcı başarıyla oluşturuldu.");
+                        Temizle();
+                    }
+                    else if (radioButton2.Checked)
+                    {
+                        yeniKullanici.Meslek = "Öğretmen";
+                        kullanicilar.Add(yeniKullanici);
+                        MessageBox.Show("Kullanıcı başarıyla oluşturuldu.");
+                        Temizle();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lütfen mesleğinizi giriniz!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Girdiğiniz şifreler aynı değil!");
+                    txtYeniSifre.Text = "";
+                    txtYeniSifreTekrar.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen kullanıcı sözleşmesini kabul edin.");
+            }
+        }
+
+        private void btnCikis_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            groupBox1.Visible = true;
+            groupBox2.Visible = true;
+            btnCikis.Visible = false;
+            MessageBox.Show("Çıkış yapıldı!");
+
+            //pictureBox1.Image.Dispose();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("C:\\Users\\izel\\source\\repos\\tugaytuna Yazilim-Egitim2025-26 main 33.Ders-FormUygulamalari\\Resources\\okul2.jpg");
+
+        }
+    }
+}
