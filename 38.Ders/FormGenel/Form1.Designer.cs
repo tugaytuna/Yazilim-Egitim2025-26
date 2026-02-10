0884573f199418c0ca342ccfe2bdd8d040d8ccba
Tugay Tuna <tugaytuna@outlook.com.tr>
2026-02-10 10:49:34 +0300

    ders38

---
+++ 38.Ders/FormGenel/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace FormGenel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(62, 102);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(320, 41);
+            this.btnBaslat.TabIndex = 0;
+            this.btnBaslat.Text = "İşlemi Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(62, 149);
+            this.progressBar1.MarqueeAnimationSpeed = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(320, 49);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGirisYap);
+            this.groupBox1.Controls.Add(this.btnKaydet);
+            this.groupBox1.Controls.Add(this.txtIsim);
+            this.groupBox1.Location = new System.Drawing.Point(432, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(681, 362);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kullanıcı Sayfası";
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(22, 65);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(228, 31);
+            this.txtIsim.TabIndex = 0;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(22, 286);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(228, 43);
+            this.btnKaydet.TabIndex = 1;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Location = new System.Drawing.Point(22, 102);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(228, 42);
+            this.btnGirisYap.TabIndex = 2;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            this.btnGirisYap.Click += new System.EventHandler(this.btnGirisYap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1152, 665);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnBaslat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.TextBox txtIsim;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnGirisYap;
+    }
+}
+
---
+++ 38.Ders/FormGenel/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormGenel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Kullanici
+        {
+            public int KullaniciID { get; set; }
+            public string KullaniciAdi { get; set; }
+            public Color Renk { get; set; }
+        }
+
+        Kullanici kullanici1 = new Kullanici() { KullaniciID = 1, KullaniciAdi = "tugay", Renk = Color.Khaki };
+        Kullanici kullanici2 = new Kullanici() { KullaniciID = 2, KullaniciAdi = "samet", Renk = Color.Sienna };
+
+        List<Kullanici> kullaniclar = new List<Kullanici>();
+
+        int loginUserId = 0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kullaniclar.Add(kullanici1);
+            kullaniclar.Add(kullanici2);
+
+
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            //if (progressBar1.Value < 100)
+            //{
+            //    progressBar1.Value += 10;
+            //}
+
+            
+
+            //for (int i = 0; i <= progressBar1.Maximum; i++)
+            //{
+            //    progressBar1.Value = i;
+            //}
+
+            //progressBar1.Value = (progressBar1.Maximum / 2);
+            progressBar1.Value = 50;
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                groupBox1.BackColor = colorDialog1.Color;
+                Kullanici loginUser = kullaniclar.Find(item => item.KullaniciID == loginUserId);
+
+                if (loginUser != null)
+                {
+                    loginUser.Renk = colorDialog1.Color;
+                }
+                
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                MessageBox.Show("Hiçbir renk seçilmedi!");
+            }
+
+        }
+
+        private void btnGirisYap_Click(object sender, EventArgs e)
+        {
+            foreach (Kullanici user in kullaniclar)
+            {
+                if (user.KullaniciAdi == txtIsim.Text)
+                {
+                    groupBox1.BackColor = user.Renk;
+                    loginUserId = user.KullaniciID;
+                }
+            }
+        }
+    }
+}
