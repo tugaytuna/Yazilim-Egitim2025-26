c373030e7260c2645e94a8c10cd54f0ad9523534
Tugay Tuna <tugaytuna@outlook.com.tr>
2026-03-17 23:01:12 +0300

    57.ders

---
+++ 57.Ders/LoginOrnek/LoginOrnek/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginOrnek.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
